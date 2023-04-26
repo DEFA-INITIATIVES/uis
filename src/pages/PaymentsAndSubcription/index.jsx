4f88f3c9d54424d10e42fa6558b00109485b85df
Bkroland19 <bkroland19@gmail.com>
2023-04-26 10:35:19 +0300

    payment large card

--- src/pages/PaymentsAndSubcription/index.jsx
+++ src/pages/PaymentsAndSubcription/index.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
-import Header from '../../components/Header'
-import AccoutSetting from '../../components/AccountSettings'
+import React from "react";
+import Header from "../../components/Header";
+import AccoutSetting from "../../components/AccountSettings";
 
 function SubcriptionAndPaymentScreen() {
-  return (
-    <div>
+	return (
+		<div>
+			<Header />
+			<div className="flex">
+				<div>
+					<AccoutSetting />
+				</div>
 
-      <Header/>
-       <div>
-        <AccoutSetting />
-       </div>
-    </div>
-  )
+				<div>
+					<div className="h-[35vh] bg-gray-700/50 w-[75vw] rounded-[25px] mt-8">
+						<div className=" text-[30px] absolute left-[474px] top-[214px] text-white font-extrabold ">
+							GO
+							<span className="ml-4 w-[146.51px] text-white px-4 text-center  h-[51px] bg-[#0049F5] rounded-[25px]">
+								PRO
+							</span>
+						</div>
+						<div className=" text-[30px] my-5 text-white font-extrabold absolute left-[474px] top-[254px]">
+							PREMIUM
+						</div>
+						<div className="text-[20px] text-white  font-medium absolute left-[474px] top-[324px]">
+							Millions of happy users <br /> choices
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default SubcriptionAndPaymentScreen
+export default SubcriptionAndPaymentScreen;
